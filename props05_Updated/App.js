c16d292f54deaaa887d73303807809f6125ef8ae
Hansamalee <154486739+Hansamalee0630@users.noreply.github.com>
2024-02-07 11:58:26 +0530

    Update App.js

--- props05_Updated/App.js
+++ props05_Updated/App.js
@@ -9,7 +9,7 @@ export default function App() {
     const cards = data.map(item => {
         return (
             <Card
-                key={}
+                key={item.id}
                 img={item.coverImg}
                 rating={item.stats.rating}
                 reviewCount={item.stats.reviewCount}

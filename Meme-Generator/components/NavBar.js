f206fdac62d3f35882320ea1187579de5393184a
Hansamalee <154486739+Hansamalee0630@users.noreply.github.com>
2024-02-11 12:45:14 +0530

    Create NavBar.js

---
+++ Meme-Generator/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function NavBar(){
+    return (
+        <nav className = "nav-style">
+            <img src = "../images/troll-face.png" className = "logo-troll"/>
+            <h2 className = "nav-title" >Meme Generator</h2>
+            <h5 className = "nav-sub-heading" >React Course - Project 03</h5>
+        </nav>
+    )
+}

ba7057845b8d6c53c7d120558f63f957d5f5697e
Hansamalee <154486739+Hansamalee0630@users.noreply.github.com>
2024-02-11 12:43:45 +0530

    Create App.js

---
+++ Meme-Generator/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+import NavBar from "./components/NavBar"
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <NavBar />
+            <Header />
+        </div>
+    )
+}

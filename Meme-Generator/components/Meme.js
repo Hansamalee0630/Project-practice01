20ef027c1b4bdf7874e4c23f022944092b603b48
Hansamalee <154486739+Hansamalee0630@users.noreply.github.com>
2024-02-11 13:35:28 +0530

    Update Meme.js
    Added labels to the input buttons.

--- Meme-Generator/components/Meme.js
+++ Meme-Generator/components/Meme.js
@@ -4,16 +4,22 @@ export default function Meme(){
     return(
         <main>
             <form className = "form" >
-                <input 
-                    type = "text" 
-                    placeholder="Top text"
-                    className = "text-01" 
-                />
-                <input 
-                    type = "text" 
-                    placeholder="Bottom text"
-                    className = "text-01" 
-                />
+                <div>
+                    <label className="form--label">Top text</label>
+                    <input 
+                        type = "text" 
+                        placeholder="shut up"
+                        className = "text-01" 
+                    />
+                </div>
+                <div>
+                    <label className="form--label">Bottom text</label>
+                    <input 
+                        type = "text" 
+                        placeholder="and take my money"
+                        className = "text-01" 
+                    />
+               </div>
                 <button 
                     className = "Button-style">
                     Get a new meme image 🖼
